118dafb0ec2e6151b04134eb5c5d66396ddba3f8
Erwin JeanLouis <erwin_jeanlouis@yahoo.com>
2015-05-23 19:07:46 -0400

    Project for DataProduct coursera

---
+++ server.R
@@ -0,0 +1,96 @@
+library(shiny)
+
+initializePredictor <- function()
+{
+    library(caret)
+    library(randomForest)
+    
+    library (datasets)
+    library ( e1071 )
+    
+    set.seed(3433)
+    
+    data ( iris )
+    inTrain <- createDataPartition(iris$Species, p = 3/4)[[1]]
+    training <- iris[ inTrain,]
+    testing <- iris[-inTrain,]
+    fitRf <- train(Species~., training, method="rf")
+    return( fitRf)
+}
+
+pieLabel <- function (a, b)
+{
+    x <- paste(a, "(")
+    x <- paste(x, as.character (b))
+    x <- paste (x, ")")
+    return ( x )
+}
+
+mlPredictor <- initializePredictor()
+correctAnswers <- 0
+incorrectAnswers <- 0
+# Define server logic required to generate and plot a random distribution
+
+shinyServer(function(input, output, clientData, session) {
+    
+    observe({
+        if (input$resetValues == 0)
+            return()
+        isolate({
+        updateNumericInput(session, "sepalLength", value = runif(1, 0.0, 5.0))            
+        updateNumericInput(session, "sepalWidth", value = runif(1, 0.0, 5.0))            
+        updateNumericInput(session, "petalLength", value = runif(1, 0.0, 5.0))            
+        updateNumericInput(session, "petalWidth", value = runif(1, 0.0, 5.0))            
+                
+    })
+    
+    })
+    
+    
+    observe({
+        if (input$verify == 0)
+            return()
+        isolate({
+            Sepal.Length <- input$sepalLength
+            Sepal.Width <- input$sepalWidth
+            Petal.Length <- input$petalLength
+            Petal.Width <- input$petalWidth
+            userData <- data.frame(Sepal.Length, Sepal.Width, Petal.Length, Petal.Width )
+            predictedresult <- as.character(predict( mlPredictor, userData )[[1]])
+            #userResult <- as.character(result[[1]])
+            petalUserPrediction <- 
+                if ( input$petalUserPrediction == 1 ) "setosa"
+                else if ( input$petalUserPrediction == 2) "versicolor"
+                else "virginica"
+            
+            
+            result <- paste(predictedresult, petalUserPrediction)
+            if (predictedresult == petalUserPrediction)
+            {
+                correctAnswers <<- correctAnswers + 1
+                output$userResult <- renderPrint({ "Great Job!!!" })
+            }
+            else
+            {
+                incorrectAnswers <<- incorrectAnswers + 1
+                output$userResult <- renderPrint({"Your choice was incorrect. Please try again!!!"})
+            }
+            
+            if (correctAnswers + incorrectAnswers > 0)
+            {
+                output$numberOfGames <-  renderPrint({ correctAnswers + incorrectAnswers })
+                output$distPlot <- renderPlot({        
+                    slices <- c(correctAnswers, incorrectAnswers)
+                    lbls <- c(pieLabel("Correct", correctAnswers),
+                          pieLabel("Incorrect", incorrectAnswers))
+                    pie(slices, labels = lbls, main="Key Performance Indicator")
+                })
+            }
+            
+        })
+        
+    })    
+
+    
+})
+
---
+++ ui.R
@@ -0,0 +1,52 @@
+library(shiny)
+
+# Define UI for application that plots random distributions 
+shinyUI(fluidPage(
+    titlePanel("Would you like to play a game..."),
+    
+    sidebarPanel(
+        p("This simple application is designed
+      to test your knowledge of petal charateristics of the iris flower. There are four important features (measured in inches) of the petal you need to understand. The petal width, petal length, sepal width and sepal length.
+      You can either choose these values yourself or ask the computer to randomly select the values.
+      Once you entered the values, you need to guess at the type of petal. Then, press the verify button and the friendy computer (called AI) will tell if you got it right.
+      The computer was trained to know the right answer based on a Random Forest machine learning using the iris dataset in R.
+      The computer keeps track of right and wrong answers and shows you a pie chart of your progress.
+      "),
+        a(href = "http://en.wikipedia.org/wiki/Iris_flower_data_set", "For more information, Click Here!"),
+        
+        plotOutput("distPlot")
+    ),
+    
+    mainPanel(
+    wellPanel(
+    fluidRow(
+        column(4,
+               numericInput("sepalLength", label = h3("Sepal Length"), value = 1)),
+    
+        column(8,
+               numericInput("sepalWidth", label = h3("Sepal Width"), min = 0, max = 5, value = 1))),
+    
+    fluidRow(
+        column(4,               
+            numericInput("petalLength", label = h3("Petal Length"), min = 0, max = 5, value = 1)),
+        column(8,
+            numericInput("petalWidth", label = h3("Petal Width"), min = 0, max = 5, value = 1))),
+    
+    actionButton("resetValues", label = "Random Petal Features")
+    ),
+    hr(),
+    
+    # Copy the chunk below to make a group of checkboxes
+    radioButtons("petalUserPrediction", label = h3("Please select the species based on the petal features above"), 
+                       choices = list("setosa" = 1, "versicolor" = 2, "virginica" = 3),
+                       selected = 1),
+    
+    fluidRow(
+        column(4,               
+            actionButton("verify", label = "Verify Answer")),
+        column(8,
+            verbatimTextOutput("userResult")))
+    
+   
+)))
+#)
